90289ed1331e508ac217da64643a8be76a40cd3d
SusanWendt <wendt.37@osu.edu>
2018-02-08 10:10:36 -0500

    testing started

--- src/test/java/battleship/DotComTest.java
+++ src/test/java/battleship/DotComTest.java
@@ -1,13 +1,16 @@
 package battleship;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class DotComTest {
-	@Test 
-	public void shouldCreateDotComClass() {
-		DotCom underTest = new DotCom();
-		
-	}
+//	@Test 
+//	public void shouldCreateDotComClass() {
+//		DotCom underTest = new DotCom();
+//		underTest.setLocaitonCells(ArrayList<String> loc);
+//		locationCells = loc;
+//	}
 	
 
 }
